5567bda92a43d0c5ce4e6b4095a3096a1108158c
FratNightGaming <jberger1@iwu.edu>
2023-02-04 01:17:34 -0600

    add task and delete task functionality; input form; task table

--- ToDoList/src/app/Components/tasks/tasks.component.ts
+++ ToDoList/src/app/Components/tasks/tasks.component.ts
@@ -9,10 +9,13 @@ import { Task } from 'src/app/Models/task';
 export class TasksComponent implements OnInit
 {
   //title not needed - delete soon
-  title:string = "Task";
+  title:string = "Tasks";
 
   tasks:Task[] = [];
-  
+
+  inputTask:string = "";
+  newTask:Task = {} as Task;
+
   constructor(){}
 
   ngOnInit(): void 
@@ -31,4 +34,52 @@ export class TasksComponent implements OnInit
       {task:"read a book", completed:true}
     ]
   }
+
+  ToggleComplete(selectedTask: Task, index: number):void
+  {
+    selectedTask.completed = !selectedTask.completed;
+
+    //this.tasks[index].completed = !this.tasks[index].completed;
+
+    //index parameter is only used with map function directly below
+
+    // this.tasks.map((v, i) => 
+    // {
+    //   if (i === index)
+    //   {
+    //     v.completed = !v.completed;
+    //   }
+    // })
+  }
+
+  DeleteTask(selectedTask: Task):void
+  {
+    for(let i = 0; i<this.tasks.length; i++)
+    {
+      if(this.tasks[i] === selectedTask)
+      {
+        this.tasks.splice(i, 1);
+      }
+    }
+  }
+
+  DeleteTask2(selectedTask: Task):void
+  {
+    this.tasks = this.tasks.filter(t => t !== selectedTask);
+  }
+
+  AddTask():void
+  {
+    this.newTask.task = this.inputTask;
+    this.newTask.completed = false;
+    this.tasks.push(this.newTask);
+    
+    //must reset newTask after submitting form
+    this.newTask = {} as Task;
+    //this.tasks.push({task:this.inputTask, completed:false});
+
+    //reset input text field
+    this.inputTask = "";
+  }
+
 }
